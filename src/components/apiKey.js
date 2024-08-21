1f9af23204eb3b04cdf19c27efac5dcc16931397
mkrenzke <krenzke@unipoin.com>
2024-08-21 18:45:02 +0200

    Make name required as it is in the next step anyway

--- src/components/apiKey.js
+++ src/components/apiKey.js
@@ -148,13 +148,13 @@ export const ApiKeyCreate = (props) => {
           defaultValue={generateApiKey()}
           size='large'
           fullWidth={true}
-          validate={[required()]}
+          validate={required()}
           readOnly={true}
         />
 
         <Row>
           {' '}
-          <TextInput source='name' size='large' />
+          <TextInput source='name' size='large' validate={required()} />
           <TextInput source='description' size='large' />
         </Row>
 
