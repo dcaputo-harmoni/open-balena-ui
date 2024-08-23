f0c5395172987cae728e7f2c008a72446cc3105a
Felix Huebner <huebner@unipoin.com>
2024-08-23 14:54:23 +0200

    update dockerfile to be based on node22-alpine. change dockerfile to run in node_env production.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const minioClient = new Minio.Client({
   endPoint: process.env.OPEN_BALENA_S3_URL.split('://')[1],
-  useSSL: process.env.OPEN_BALENA_S3_URL.includes('https') ? true : false,
+  useSSL: !!process.env.OPEN_BALENA_S3_URL.includes('https'),
   accessKey: process.env.OPEN_BALENA_S3_ACCESS_KEY,
   secretKey: process.env.OPEN_BALENA_S3_SECRET_KEY,
 });
